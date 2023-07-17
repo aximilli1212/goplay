d08a565c8800dbc860324d94691437f492484d5f
Emmanuel Sackey <aximilli1212@gmail.com>
2023-07-17 04:48:44 +0000

    delare var, shorthand:useage 233

---
+++ packagefmt/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Akumia"
+	fmt.Println("Hello playground, I am: ", name)
+
+	//Note that fmt.Println() can also accept expressions
+	a, b := 4, 6
+	fmt.Println("Sum: ", a+b, "Mean Value: ", (a+b)/2)
+
+	//Note that there is fmt.Printf() which allows you to interpolate with placeholders
+	fmt.Printf("x is %d , y is %f, stringa is: %s", 5, 4.5, "shook")
+}
