92d4cdbf590dd0d12e85f75c05bf1593e88b5555
Emmanuel Sackey <aximilli1212@gmail.com>
2023-07-17 05:52:07 +0000

    more constant rules added

--- constants/main.go
+++ constants/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	const days = 45
@@ -14,5 +17,16 @@ func main() {
 		min3
 	)
 
-	fmt.Println(min1, min2, min3) //NB: all the declared consts are assigned the first assigned vlaue: 400 by default
+	fmt.Println(min1, min2, min3) //NB: all the declared consts are assigned the
+	// first assigned vlaue: 400 by default
+	infVal := math.Pow(3, 4)
+	fmt.Println(infVal)
+
+	//Constant Rules
+	//1. You cannot change a constant
+	//2. You cannot initialize a const at runtime
+	//const power = math.Pow(2,3) will throw an error
+	//3. You cannot use a constant to initialize a variable
+	//4. You can initialize a const with a builtIn func eg: len
+
 }
