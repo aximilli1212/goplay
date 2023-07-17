14d381b8d5473fb8e05e5202d233f923bb8c710e
Emmanuel Sackey <aximilli1212@gmail.com>
2023-07-17 04:41:50 +0000

    delare var, shorthand:useage 2

--- variables/main.go
+++ variables/main.go
@@ -7,5 +7,10 @@ func main() {
 	fmt.Println("my age is: ", age)
 	name := "Andrea"
 	fmt.Println("my name is: ", name)
-
+	school, job := "KNUT", "Programmer"
+	fmt.Println(school, job)
+	school = "Uiversity of Cape Coast"
+	fmt.Println("newSchool::", school)
+	var money string
+	fmt.Println("money made", money)
 }
