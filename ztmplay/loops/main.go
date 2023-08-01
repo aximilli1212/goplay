21c5fc2782c01ddbe919c68b4078180926db6853
Emmanuel Sackey <aximilli1212@gmail.com>
2023-08-01 05:12:59 +0000

    finalize fizzbuzz

--- ztmplay/loops/main.go
+++ ztmplay/loops/main.go
@@ -17,12 +17,15 @@ import "fmt"
 func main() {
 
 	for i := 1; i <= 50; i++ {
-		if i%3 == 0 {
-			fmt.Println("Fizz", i)
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz:", i)
+		} else if i%3 == 0 {
+			fmt.Println("Fizz:", i)
 		} else if i%5 == 0 {
 			fmt.Println("Buzz:", i)
-		} else if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuzz:", i)
+		} else {
+			fmt.Println(i)
 		}
 	}
+
 }
