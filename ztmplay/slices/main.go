c780d67318d74eda1c9285aea26ec079bb42ab97
Emmanuel Sackey <aximilli1212@gmail.com>
2023-08-01 09:14:02 +0000

    add arrays exercise

---
+++ ztmplay/slices/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Part string
+
+func main() {
+
+	//* Using a slice, create an assembly line that contains type Part
+	//* Create a function to print out the contents of the assembly line
+	//* Perform the following:
+	//  - Create an assembly line having any three parts
+	assemblyLine := []Part{"Screws", "Bolts", "Nuts"}
+	fmt.Println(assemblyLine)
+
+	//  - Add two new parts to the line
+	//  - Slice the assembly line, so it contains only the two new parts
+	//  - Print out the contents of the assembly line at each step
+	//--Notes:
+	//* Your program output should list 3 parts, then 5 parts, then 2 parts
+
+}
