7a7e7bb91b369022295f61cebae18e246755c035
Emmanuel Sackey <aximilli1212@gmail.com>
2023-08-10 05:13:19 +0000

    Add demo for parking lot set and release receiver functions

---
+++ ztmplay/receivers/demo.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type Space struct {
+	occupied bool
+}
+
+type ParkingLot struct {
+	spaces []Space
+}
+
+func occupySpace(lot *ParkingLot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
+func main() {
+
+	myLot := ParkingLot{spaces: make([]Space, 10)}
+
+	fmt.Println("initial:", myLot)
+
+	myLot.occupySpace(1)
+	occupySpace(&myLot, 2)
+	fmt.Println("After occupied:", myLot)
+
+}
