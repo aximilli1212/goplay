114509b7178a901e7399357ba37483780f9f1a4f
Emmanuel Sackey <aximilli1212@gmail.com>
2023-08-11 04:48:56 +0000

    Add package management with organization

---
+++ ztmplay/packages/display/display.go
@@ -0,0 +1,7 @@
+package display
+
+import "fmt"
+
+func Display(display string) {
+	fmt.Println("Message: ", display)
+}
---
+++ ztmplay/packages/msg/emote.go
@@ -0,0 +1,8 @@
+package msg
+
+import "fmt"
+
+func Mixin() {
+	fmt.Println("We are mixins")
+	Mongoose()
+}
